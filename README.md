c02c629c8f8c9d6d1ee95cacdeb942dc942e3cca
Adithya Satyanarayana <adithya.satyanarayana@yahoo.com>
2022-02-22 23:58:29 +0530

    Dictionaries in python

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Repository to keep track of python practice exercises.
 
 Feb 20th, 2022 - Lists. <br />
 Feb 21st, 2022 - Tuples. <br />
+Feb 22nd, 2022 - Dictionaries. <br />
---
+++ python-dictionaries/dictionaries.py
@@ -0,0 +1,5 @@
+# Dictionaries are unordered and mutable.
+# Consists of a collection of key-value pairs.
+
+user_details = {"Last Name":"James", "First Name":"LeBron", "Team":"Los Angeles Lakers", "Position":"Small Forward"}
+print(user_details)
